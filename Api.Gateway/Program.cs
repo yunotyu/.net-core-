6b1540bf0e170dca562472b06125c2554fe900c9
yunotyu <623573553@qq.com>
2020-04-22 08:38:39 +0800

    添加ocelot

---
+++ Api.Gateway/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Gateway.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            //添加Ocelot的配置文件
+            .ConfigureAppConfiguration((hostingContext, builder) =>
+            {   //获取项目的根目录
+                builder.SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                .AddJsonFile("Ocelot.json");
+            }) 
+                .UseStartup<Startup>();
+    }
+}
